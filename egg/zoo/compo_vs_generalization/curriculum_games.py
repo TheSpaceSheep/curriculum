new masking + fix bugs

egg/zoo/compo_vs_generalization/curriculum_games.py
@@ -55,7 +55,7 @@ class GraduallyRevealAttributes(CurriculumGameWrapper):
             mode: str,
             initial_n_unmasked: int=1
         ):
-        valid_modes = ['left_to_right', 'random']
+        valid_modes = ['left_to_right', 'random', 'dedicated_value']
         if mode not in valid_modes:
             raise ValueError(f"Invalid mode {mode}. mode should be in {valid_modes}")
 
@@ -63,7 +63,7 @@ class GraduallyRevealAttributes(CurriculumGameWrapper):
         self.n_attributes = n_attributes
         self.n_values = n_values
         self.mode = mode
-        self.n_unmasked = initial_n_unmasked
+        self.n_unmasked = min(initial_n_unmasked, n_attributes)
 
 
     def forward(self, sender_input, labels, receiver_input=None, aux_input=None):
@@ -72,21 +72,24 @@ class GraduallyRevealAttributes(CurriculumGameWrapper):
         if self.mode == 'left_to_right':
             idxs_to_mask = torch.arange(self.n_unmasked, self.n_attributes, dtype=torch.long)
             idxs_to_mask = idxs_to_mask.expand(batch_size, idxs_to_mask.shape[0])
-        elif self.mode == 'random':
+        elif self.mode == 'random' or self.mode == 'dedicated_value':
             mask_probability = torch.ones((batch_size, self.n_attributes))/self.n_attributes
             n_masks = self.n_attributes - self.n_unmasked
             # multinomial throws an error when we try to sample 0 elements
             # so we manually specify an empty idxs
-            print(n_masks)
             if n_masks == 0:
-                idxs_to_mask = torch.tensor([])
+                idxs_to_mask = torch.tensor([[]]*batch_size)
             else:
                 idxs_to_mask = torch.multinomial(mask_probability,
-                        self.n_attributes - self.n_unmasked,
+                        n_masks,
                         replacement=False)
 
         idxs_to_mask = idxs_to_mask.to(sender_input.device)
-        sender_input = mask_attributes(sender_input, idxs_to_mask, self.n_attributes, self.n_values)
+        sender_input = mask_attributes(sender_input,
+                idxs_to_mask,
+                self.n_attributes,
+                self.n_values,
+                mask_by_last_value=(self.mode=='dedicated_value'))
 
         # pass indices to mask to the loss function through aux_input
         if aux_input is None: